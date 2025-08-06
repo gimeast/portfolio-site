9aeac4e211b6d5e989c08701087371f8d9bb2f88
gimeast <slasnrndu@gmail.com>
2025-08-07 00:01:59 +0900

    중첩된 라우터 구조도 적용 가능하도록 확장

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -2,14 +2,19 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/Layout.jsx";
 import navRoutes from "./routes.js";
 
+const createRoutes = (routes) => {
+    return routes.map((route) => ({
+        path: route.path,
+        element: <route.component />,
+        ...(route.children && { children: createRoutes(route.children) })
+    }));
+};
+
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Layout />,
-        children: navRoutes.map((route) => ({
-            path: route.path,
-            element: <route.component />,
-        })),
+        children: createRoutes(navRoutes),
     },
 ]);
 
--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,16 @@ import Contact from "../components/Contact.jsx";
 const navRoutes = [
     { path: "/", label: "홈", component: Home },
     { path: "/project", label: "프로젝트", component: Project },
+    /*중첩 구조인 경우
+    {
+          path: "/project",
+          label: "프로젝트",
+          component: Project,
+          children: [
+              { path: "web", label: "웹 프로젝트", component: WebProject },
+              { path: "mobile", label: "모바일 앱", component: MobileProject }
+          ]
+      }*/
     { path: "/about", label: "소개", component: About },
     { path: "/contact", label: "연락처", component: Contact },
 ];
